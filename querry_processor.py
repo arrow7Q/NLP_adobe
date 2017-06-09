1e74ff6a84fe48dfec58aa4c04236b5097ec58c3
arrow7Q <utkarshdce05@gmail.com>
2017-06-09 19:34:55 +0530

    Add files via upload

--- querry_processor.py
+++ querry_processor.py
@@ -7,30 +7,46 @@ ques=sys.stdin.readline()
 
 words= word_tokenize(ques)
 tagged=pos_tag(words)
-#namedent=ne_chunk(tagged)
-
-#processing intent
+namedent=ne_chunk(tagged)
 
 print tagged
 
-intent={'WRB':{'How':'Process', 'Where':'Place', 'Why':'Reason','When':'Time'},
-         'WP':{'What':'Information','Who':'Person', 'Whom':'Person'},
-         'WDT':{'What':'Information','Which':'Information'}
+
+intent={'WRB':{'how':'Process', 'where':'Place', 'why':'Reason','when':'Time'},
+         'WP':{'what':'Information','who':'Person', 'whom':'Person'},
+         'WDT':{'what':'Information','which':'Information'}
          }
 
-#print intent['WRB']['How']
+#processing intent
 
+def mode():
+  for items in tagged:
+      if any ( [ items[1] == 'WRB', items[1] == 'WP', items[1] == 'WDT'] ):
+          return intent[items[1]][items[0]]
+inten = mode()
+print inten
+
+
+        
 #namedent.draw()
 
 #tag_list=[]
-#tag_list=namedent.pos()
+tag_list=namedent.pos()
 #print tag_list
 #print tag_list[0]
 
-print intent[tagged[0][1]][tagged[0][0]]
+#print intent[tagged[0][1]][tagged[0][0].lower()]
 
 
-                
+
+#doing chunking and noun phrase extraction
+grammar = "NP: {<JJ>*<NN>}"
+
+cp = nltk.RegexpParser(grammar)
+
+result=cp.parse(tagged)
+#print result 
+
 
 
 
