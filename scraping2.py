b76c1491cc8d9a5eb47594bd62d9a0ecd3e1cce7
arrow7Q <utkarshdce05@gmail.com>
2017-06-12 20:17:28 +0530

    Add files via upload

--- scraping2.py
+++ scraping2.py
@@ -1,21 +1,57 @@
-import xml.etree.ElementTree as ET
+import json
+import os
+from lxml import etree
+import nltk
+from nltk import word_tokenize, sent_tokenize ,pos_tag ,ne_chunk
+from nltk.corpus import stopwords
+
+path='/Users/utkarsh/codes/Help-bot/samples'
 
 
+docmap=[]
 
-path='dita/frm_getstarted_gs.xml'
 
-
-tree=ET.parse(path)
-root=tree.getroot()
+for filename in os.listdir(path):
+    if not filename.endswith('.xml'):continue
+    fullname = os.path.join(path,filename)
+    tree = etree.parse(fullname)
+    root = tree.getroot()
     
-print root.tag
-print root.attrib
-
-for child in root:
-  for i in child.iter('p'):
-    print i.text
-  print '\n'
-
-
-    
+    for child in root:
+            newElement={}
+            newElement['filename']=filename
+            newElement['type']=child.tag
+            newElement['text']=''.join(child.itertext()).replace('\n','')
+            docmap.append(newElement)
         
+#print json.dumps(docmap,indent=2)
+#for elem in docmap:
+    #print elem
+
+
+
+from nltk.corpus import stopwords
+stop_words=set(stopwords.words("english"))
+
+
+for elem in docmap:
+    if('body' in elem['type']):
+        value=elem['text']
+        #value=''.join(value.split())
+        tokenized=word_tokenize(value.strip())
+        filtered_value=[w for w in tokenized if not w in stop_words]
+        tagged=pos_tag(tokenized)
+        print filtered_value
+        
+
+    
+
+
+
+
+    
+
+               
+    
+
+
